dd7feb8bbb2be58a9d90cfdd1bb2e838972090b1
belalelgebaly <belalelgebaly4@gmail.com>
2023-02-02 00:55:48 +0200

    setup

---
+++ src/components/data.js
@@ -0,0 +1,33 @@
+export const icons = [
+    {
+        name: "Interstellar",
+        poster: "./images/posters/interstellar.jpg",
+        body: "Interstellar is a 2014 epic science fiction film co-written, directed and produced by Christopher Nolan. It stars Matthew McConaughey, Anne Hathaway, Jessica Chastain, Bill Irwin, Ellen Burstyn, Matt Damon, and Michael Caine. Set in a dystopian future where humanity is struggling to survive, the film follows a group of astronauts who travel through a wormhole near Saturn in search of a new home for mankind.",
+        caption: "A team of explorers travel through a wormhole in space in an attempt to ensure humanity's survival.",
+        video: "/videos/interstellar.mp4",
+        // "https://media.giphy.com/media/gMH6OpOW0II80/giphy.m4v"
+        background: "./images/backgrounds/interstellar.jpg",
+        images: [
+            "./images/sups/interstellar.png",
+            "./images/sups/interstellar1.jpg",
+            "./images/sups/interstellar2.jpg",
+            "./images/sups/interstellar3.jpg",
+        ],
+        id: 0
+    },
+    {
+        name: "The Godfather",
+        poster: "./images/posters/godfather.jpg",
+        body: "The Godfather is a 1972 American crime film directed by Francis Ford Coppola, who co-wrote the screenplay with Mario Puzo, based on Puzo's best-selling 1969 novel of the same name. The film stars Marlon Brando, Al Pacino, James Caan, Richard Castellano, Robert Duvall, Sterling Hayden, John Marley, Richard Conte, and Diane Keaton.",
+        caption: "The Godfather is regarded as one of the greatest and most influential films ever made.",
+        video: "/videos/godfather.mp4",
+        background: "./images/backgrounds/godfather.jpg",
+        images: [
+            "./images/sups/godfather.jpg",
+            "./images/sups/godfather1.jpg",
+            "./images/sups/godfather2.jpg",
+            "./images/sups/godfather3.webp",
+        ],
+        id: 1
+    },
+];
---
+++ src/components/sub.js
@@ -0,0 +1,9 @@
+const Sub = () => {
+    return ( 
+        <div className="sub">
+            <h1>Sub</h1>
+        </div>
+     );
+}
+ 
+export default Sub;
